17ba037f0d08aeeca468cc451241aa452a06a32c
ThuleMalta <thulemalta@outlook.com>
2015-04-25 16:02:32 +0200

    First push
    A really bugged version and no functionalities.

---
+++ Main.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace modbrowser
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nouveauToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aProposToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modBrowserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minecraftToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modlist = new System.Windows.Forms.ListBox();
+            this.modInfo = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.modDescription = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.modIcon = new System.Windows.Forms.PictureBox();
+            this.modAuthor = new System.Windows.Forms.Label();
+            this.modTitle = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.modInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.modIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Roboto Condensed", 9F);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nouveauToolStripMenuItem,
+            this.aProposToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nouveauToolStripMenuItem
+            // 
+            this.nouveauToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.modToolStripMenuItem});
+            this.nouveauToolStripMenuItem.Name = "nouveauToolStripMenuItem";
+            this.nouveauToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.nouveauToolStripMenuItem.Text = "Nouveau";
+            // 
+            // modToolStripMenuItem
+            // 
+            this.modToolStripMenuItem.Name = "modToolStripMenuItem";
+            this.modToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
+            this.modToolStripMenuItem.Text = "Mod";
+            // 
+            // aProposToolStripMenuItem
+            // 
+            this.aProposToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.modBrowserToolStripMenuItem,
+            this.minecraftToolStripMenuItem});
+            this.aProposToolStripMenuItem.Name = "aProposToolStripMenuItem";
+            this.aProposToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.aProposToolStripMenuItem.Text = "Paramètres";
+            // 
+            // modBrowserToolStripMenuItem
+            // 
+            this.modBrowserToolStripMenuItem.Name = "modBrowserToolStripMenuItem";
+            this.modBrowserToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.modBrowserToolStripMenuItem.Text = "ModBrowser";
+            // 
+            // minecraftToolStripMenuItem
+            // 
+            this.minecraftToolStripMenuItem.Name = "minecraftToolStripMenuItem";
+            this.minecraftToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.minecraftToolStripMenuItem.Text = "Minecraft";
+            // 
+            // modlist
+            // 
+            this.modlist.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(85)))), ((int)(((byte)(72)))));
+            this.modlist.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.modlist.Font = new System.Drawing.Font("Roboto Condensed", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modlist.ForeColor = System.Drawing.Color.White;
+            this.modlist.FormattingEnabled = true;
+            this.modlist.ItemHeight = 23;
+            this.modlist.Location = new System.Drawing.Point(12, 50);
+            this.modlist.Name = "modlist";
+            this.modlist.Size = new System.Drawing.Size(157, 391);
+            this.modlist.TabIndex = 1;
+            this.modlist.SelectedIndexChanged += new System.EventHandler(this.modSelected);
+            // 
+            // modInfo
+            // 
+            this.modInfo.BackColor = System.Drawing.SystemColors.Control;
+            this.modInfo.Controls.Add(this.button3);
+            this.modInfo.Controls.Add(this.modDescription);
+            this.modInfo.Controls.Add(this.button2);
+            this.modInfo.Controls.Add(this.button1);
+            this.modInfo.Controls.Add(this.modIcon);
+            this.modInfo.Controls.Add(this.modAuthor);
+            this.modInfo.Controls.Add(this.modTitle);
+            this.modInfo.Font = new System.Drawing.Font("Roboto Condensed", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modInfo.Location = new System.Drawing.Point(175, 12);
+            this.modInfo.Name = "modInfo";
+            this.modInfo.Size = new System.Drawing.Size(559, 537);
+            this.modInfo.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(195)))), ((int)(((byte)(74)))));
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(159)))), ((int)(((byte)(56)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(505, 413);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(38, 28);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "+1";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.plusoneButton);
+            // 
+            // modDescription
+            // 
+            this.modDescription.BackColor = System.Drawing.SystemColors.Control;
+            this.modDescription.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.modDescription.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.modDescription.Location = new System.Drawing.Point(19, 104);
+            this.modDescription.Name = "modDescription";
+            this.modDescription.ReadOnly = true;
+            this.modDescription.Size = new System.Drawing.Size(524, 303);
+            this.modDescription.TabIndex = 7;
+            this.modDescription.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(193)))), ((int)(((byte)(7)))));
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(160)))), ((int)(((byte)(0)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Roboto Condensed", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(426, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 29);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "METTRE À JOUR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.updateButton);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(67)))), ((int)(((byte)(54)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(47)))), ((int)(((byte)(47)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Roboto Condensed", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(426, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "DÉSINSTALLER";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.uninstall);
+            // 
+            // modIcon
+            // 
+            this.modIcon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(85)))), ((int)(((byte)(72)))));
+            this.modIcon.Location = new System.Drawing.Point(19, 34);
+            this.modIcon.Name = "modIcon";
+            this.modIcon.Size = new System.Drawing.Size(64, 64);
+            this.modIcon.TabIndex = 4;
+            this.modIcon.TabStop = false;
+            // 
+            // modAuthor
+            // 
+            this.modAuthor.AutoSize = true;
+            this.modAuthor.Font = new System.Drawing.Font("Roboto Condensed Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modAuthor.Location = new System.Drawing.Point(102, 70);
+            this.modAuthor.Name = "modAuthor";
+            this.modAuthor.Size = new System.Drawing.Size(34, 15);
+            this.modAuthor.TabIndex = 1;
+            this.modAuthor.Text = "Auteur";
+            // 
+            // modTitle
+            // 
+            this.modTitle.AutoSize = true;
+            this.modTitle.Font = new System.Drawing.Font("Roboto Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modTitle.Location = new System.Drawing.Point(99, 38);
+            this.modTitle.Name = "modTitle";
+            this.modTitle.Size = new System.Drawing.Size(57, 32);
+            this.modTitle.TabIndex = 0;
+            this.modTitle.Text = "Titre";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(85)))), ((int)(((byte)(72)))));
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.modlist);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.modInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "modbrowser";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.modInfo.ResumeLayout(false);
+            this.modInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.modIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nouveauToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aProposToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modBrowserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minecraftToolStripMenuItem;
+        private System.Windows.Forms.ListBox modlist;
+        private System.Windows.Forms.Panel modInfo;
+        private System.Windows.Forms.Label modAuthor;
+        private System.Windows.Forms.Label modTitle;
+        private System.Windows.Forms.PictureBox modIcon;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox modDescription;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,129 @@
+﻿#region Using statements
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+#endregion
+
+namespace modbrowser
+{
+    public partial class Main : Form
+    {
+        // modbrowser's path
+        public string mbpath;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            // Load mods
+            ListMods();
+        }
+
+        /**
+         * TODO : Fully uninstalls a mod.
+         */
+        private void uninstall(object sender, EventArgs e)
+        {
+            // Shows TODO message
+            MessageBox.Show("Fonctionnalité en cours de développement.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        /**
+         * TODO : Puts a +1 to the specified mod.
+         */
+        private void plusoneButton(object sender, EventArgs e)
+        {
+            // Shows TODO message
+            MessageBox.Show("Fonctionnalité en cours de développement.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        /**
+         * TODO : Updates the selected mod if necessary.
+         */
+        private void updateButton(object sender, EventArgs e)
+        {
+            // Shows TODO message
+            MessageBox.Show("Fonctionnalité en cours de développement.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        /**
+         * When a mod is selected, puts mod info in the info panel.
+         */
+        private void modSelected(object sender, EventArgs e)
+        {
+            updateModInfo(mbpath + "/mods/" + modlist.SelectedItem + ".ssf");
+        }
+
+        #region Functions
+        /**
+         * Lists modbrowser-known mods.
+         * @return null
+         */
+        public void ListMods()
+        {
+            // For each file in instances path...
+            foreach (string file in System.IO.Directory.EnumerateFiles(mbpath + "/mods/"))
+            {
+                string[] modmeta = DecodeSSF(mbpath + file);
+                modlist.Items.Add(modmeta[0]);
+            }
+        }
+
+        /**
+         * Decodes a *.SSF file.
+         * @return the decoded array
+         */
+        public string[] DecodeSSF(string filepath)
+        {
+            string[] decoded = new string[1];
+            try
+            {
+                // Prepares the DataSet and the DataTable
+                DataSet ds = new DataSet("smartsaveformat");
+                ds.ReadXml(filepath);
+                DataTable dt = ds.Tables["datas"];
+
+                // Prepares the array to return
+                decoded = new string[dt.Columns.Count];
+
+                // Puts saved variables on the array to return
+                decoded[0] = dt.Rows[0]["col_name"].ToString();
+                decoded[1] = dt.Rows[0]["col_author"].ToString();
+                decoded[2] = dt.Rows[0]["col_version"].ToString();
+                decoded[3] = dt.Rows[0]["col_description"].ToString();
+                decoded[4] = dt.Rows[0]["col_image-url"].ToString();
+
+
+            }
+            catch (Exception e)
+            {
+                // Shows error MessageBox
+                MessageBox.Show("Erreur lors du chargement des informations liées au mod. Cela peut arriver si le fichier est corrompu ou n'a pas le bon format.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Une erreur a eu lieu. L'application va maintenant se fermer.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Exit();
+            }
+
+            // Returns the array
+            return decoded;
+        }
+
+        public void updateModInfo(string path)
+        {
+            // Gets mod info
+            string[] modmeta = DecodeSSF(path);
+
+            // Shows mod info
+            modTitle.Text = modmeta[0];
+            modAuthor.Text = modmeta[1] + " / " + modmeta[2];
+            modDescription.Text = modmeta[3];
+        }
+        #endregion
+    }
+}
